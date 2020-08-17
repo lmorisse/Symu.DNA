851a1c6f3e91067677838dde7aa11b6d276eb17c
lmorisse <laurent.morisseau@gmail.com>
2020-08-17 09:14:40 +0200

    Create solution

---
+++ SourceCode/ONA/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ONA
+{
+    public class Class1
+    {
+    }
+}
---
+++ SourceCode/ONATests/Class1Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ONA;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ONA.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void testTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
