f993c65e712fc20415669adcca1f9bff9411c66e
lmorisse <laurent.morisseau@gmail.com>
2020-08-25 10:25:38 +0200

    Move ResourceNetwork to Symu.DNA

---
+++ SourceCode/SymuDNA/Resources/ResourceCollection.cs
@@ -0,0 +1,65 @@
+﻿#region Licence
+
+// Description: SymuBiz - Symu
+// Website: https://symu.org
+// Copyright: (c) 2020 laurent morisseau
+// License : the program is distributed under the terms of the GNU General Public License
+
+#endregion
+
+#region using directives
+
+using System;
+using System.Collections.Generic;
+using Symu.Common.Interfaces.Entity;
+
+#endregion
+
+namespace Symu.DNA.Resources
+{
+    /// <summary>
+    ///     List of all the resources 
+    ///     Used by ResourceNetwork
+    /// </summary>
+    public class ResourceCollection
+    {
+        /// <summary>
+        ///     Key => ResourceId
+        ///     Values => List of Resources
+        /// </summary>
+        public List<IResource> List { get; } = new List<IResource>();
+
+        public void Add(IResource resource)
+        {
+            if (!Contains(resource))
+            {
+                List.Add(resource);
+            }
+        }
+
+        public bool Contains(IResource resource)
+        {
+            if (resource is null)
+            {
+                throw new ArgumentNullException(nameof(resource));
+            }
+
+            return Exists(resource.Id);
+        }
+
+        public IResource Get(IId resourceId)
+        {
+            return List.Find(k => k.Id.Equals(resourceId));
+        }
+
+        public bool Exists(IId resourceId)
+        {
+            return List.Exists(k => k.Id.Equals(resourceId));
+        }
+
+        public void Clear()
+        {
+            List.Clear();
+        }
+    }
+}
---
+++ SourceCode/SymuDNATests/Classes/TestResourceUsage.cs
@@ -0,0 +1,36 @@
+﻿#region Licence
+
+// Description: SymuBiz - Symu
+// Website: https://symu.org
+// Copyright: (c) 2020 laurent morisseau
+// License : the program is distributed under the terms of the GNU General Public License
+
+#endregion
+
+using System;
+using Symu.DNA.Resources;
+
+namespace SymuDNATests.Classes
+{
+    /// <summary>
+    /// Default implementation of IResourceUsage
+    /// </summary>
+    public class TestResourceUsage : IResourceUsage
+    {
+        public TestResourceUsage(byte usage)
+        {
+            Usage = usage;
+        }
+        public byte Usage { get; }
+        public bool Equals(IResourceUsage resourceUsage)
+        {
+            if (resourceUsage == null)
+            {
+                throw new ArgumentNullException(nameof(resourceUsage));
+            }
+
+            return resourceUsage is TestResourceUsage usage &&
+                   Usage == usage.Usage;
+        }
+    }
+}
